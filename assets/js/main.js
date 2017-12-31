a1face6290ac44d710c8873b775fe15f5de586f7
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-12-31 16:13:17 +0000

    fix javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,14 @@
 const domready = require('domready');
 
 domready(() => {
-    document.getElementsByClassName('obfuscated-email').forEach((element) => {
-        element.addEventListener('click', function(event) {
-            var email = element.dataset.email;
-            window.location = 'mailto:' + email;
-        })
-    })
+    const elements = document.getElementsByClassName('obfuscated-email');
+    for (let i in elements) {
+        if (Object.prototype.hasOwnProperty.call(elements, i)) {
+            const element = elements[i];
+            element.addEventListener('click', (event) => {
+                const email = element.dataset.email;
+                window.location = `mailto:${email}`;
+            })
+        }
+    }
 });
