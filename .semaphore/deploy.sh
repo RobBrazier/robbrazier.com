07a4c6e0ada7129235c0ccad00fea11d86bef282
Rob Brazier <RobBrazier@users.noreply.github.com>
2021-02-01 18:53:59 +0000

    fix removal after mirroring

--- .semaphore/deploy.sh
+++ .semaphore/deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+ftp_url="ftp://$BUNNYCDN_USERNAME:$BUNNYCDN_PASSWORD@storage.bunnycdn.com"
 
-lftp -c "set ftp:list-options -a;
-open ftp://$BUNNYCDN_USERNAME:$BUNNYCDN_PASSWORD@storage.bunnycdn.com;
-lcd public
-mirror --reverse --delete --use-cache --verbose --allow-chown --parallel=2"
+cd public
+
+lftp "$ftp_url" -e "mirror --dry-run --reverse --delete; bye" > ../to-delete.txt
+
+to_delete=$(grep rm ../to-delete.txt | awk -F"$BUNNYCDN_USERNAME/" '{ print $2 }')
+
+lftp "$ftp_url" -e "mirror --reverse --verbose --parallel=2; bye"
+
+lftp_command=""
+for f in $to_delete; do
+    lftp_command="rm -r $f; $lftp_command"
+done
+lftp "$ftp_url" -e "$lftp_command bye"
