56f668dd50b4b5a4cd67ac62bbd98828b1b11334
Rob Brazier <RobBrazier@users.noreply.github.com>
2021-02-03 18:15:14 +0000

    handle updates better

--- .semaphore/deploy.sh
+++ .semaphore/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -exo pipefail
+
 ftp_url="ftp://$BUNNYCDN_USERNAME:$BUNNYCDN_PASSWORD@storage.bunnycdn.com"
 
 cd public
@@ -6,14 +8,18 @@ cd public
 mkdir -p bunnycdn_errors
 mv 404.html bunnycdn_errors/
 
-lftp "$ftp_url" -e "mirror --dry-run --reverse --delete; bye" > ../to-delete.txt
+ftp_options="--reverse --no-perms --transfer-all --overwrite"
 
-to_delete=$(grep rm ../to-delete.txt | awk -F"$BUNNYCDN_USERNAME/" '{ print $2 }')
+lftp "$ftp_url" -e "mirror --dry-run $ftp_options --delete; bye" > ../to-delete.txt
 
-lftp "$ftp_url" -e "mirror --reverse --verbose --parallel=2; bye"
+to_delete=$(grep rm ../to-delete.txt | awk -F"$BUNNYCDN_USERNAME/" '{ print $2 }' || true)
+
+lftp "$ftp_url" -e "mirror $ftp_options --verbose; bye"
 
 lftp_command=""
 for f in $to_delete; do
     lftp_command="rm -r $f; $lftp_command"
 done
-lftp "$ftp_url" -e "$lftp_command bye"
+if [ -n "$lftp_command" ]; then
+    lftp "$ftp_url" -e "$lftp_command bye"
+fi
